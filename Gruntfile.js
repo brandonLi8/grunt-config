262a5d2889bdfb3d2ffbd0917b016b07b51d16e1
Brandon Li <brandon.li820@icloud.com>
2019-08-10 12:00:55 -0600

    Add grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+// Copyright © 2019 Brandon Li. All rights reserved.
+
+/**
+ * Grunt configuration file for the project.
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+'use strict';
+
+module.exports = require( './grunt/Gruntfile.js' );
