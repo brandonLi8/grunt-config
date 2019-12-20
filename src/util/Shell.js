a10ccdb9e942a86991277dc27c306be84fa22f89
Brandon Li <brandon.li820@icloud.com>
2019-12-19 21:44:06 -0700

    #19 Add Shell skeleton

---
+++ src/util/Shell.js
@@ -0,0 +1,19 @@
+// Copyright © 2019 Brandon Li. All rights reserved.
+
+/**
+ * A grunt-specific Unix Shell encapsulation to wrap and execute command-line commands both asynchronously and synchronously.
+ *
+ * @author Brandon Li <brandon.li820@gmail.com>
+ */
+
+module.exports = ( () => {
+
+  // modules
+  const Util = require( './Util' );
+
+  class Shell {
+  }
+
+
+  return Shell;
+} )();
