4912d4165179c6a14ce6b1804b6d2253e90e3c4b
Brandon Li <brandon.li820@icloud.com>
2019-12-22 18:00:05 -0700

    #19 add generator module

--- src/Linter.js
+++ src/Linter.js
@@ -12,7 +12,8 @@
  * support different file types in the future.
  *
  * The configuration for ESlint can be found ../eslint/.eslintrc.js (relative to this file). It uses some default rules
- * (see documentation: https://eslint.org/docs/rules/) but also implements custom rules (see ../eslint/rules).
+ * (see documentation: https://eslint.org/docs/rules/) but also implements custom rules (see ../eslint/rules). In
+ * order to use the .eslintrc.js configuration, it must be specified in package.json.
  *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
@@ -22,6 +23,7 @@ module.exports = ( () => {
 
   // modules
   const eslint = require( 'eslint' );
+  const Generator = require( './Generator' );
   const grunt = require( 'grunt' );
   const md5 = require( 'md5' );
   const path = require( 'path' );
@@ -33,7 +35,67 @@ module.exports = ( () => {
 
   class Linter {
 
+    /**
+     * Lints the entire root directory that invoked the command, using the ESlint configuration defined in
+     * ../eslint/.eslintrc.js (relative to this file). Uses some custom rules (see ../eslint/rules). Will error if
+     * package.json doesn't contain something like:
+     *   "eslintConfig": {
+     *      "extends": "./node_modules/@brandonli8/grunt-config/eslint/.eslintrc.js"
+     *   }
+     * @public
+     *
+     * @param {boolean} useCache - indicates if the ESlint cache should be used. Caching doesn't re-lint files that
+     *                             haven't changed.
+     * @returns {Object} - ESlint report object
+     */
+    static eslint( useCache ) {
+      Util.assert( typeof useCache === 'boolean', `invalid useCache: ${ useCache }` );
 
+      // Check that package.json was implemented correctly as described above.
+      Util.assert( Generator.parseNestedPackageValue( [ 'eslintConfig', 'extends' ] ), 'ESLINTRC.JS' );
+
+      // The path to grunt-config relative to the root directory of which the command was invoked.
+      const gruntConfigPath = path.dirname( __dirname );
+
+      // The current working directory that is being linted, which is the root directory that invoked the command.
+      const currentWorkingDirectory = path.dirname( process.cwd() );
+
+      // Use the Node.js ESLint API. See https://eslint.org/docs/developer-guide/nodejs-api.
+      const cli = new eslint.CLIEngine( {
+
+        // Current working directory - Lints the entire root directory that invoked the command
+        cwd: currentWorkingDirectory,
+
+        // Caching checks changed files or when the list of rules is changed. Changing the implementation of
+        // a custom rule does not invalidate the cache. Caches are formated in .eslintcache files inside of
+        // ../eslint/cache/**.eslintcache (relative to this file).
+        cache: useCache,
+
+        // Indicates where to store the target-specific cache file. Use md5 to hash the file name. This path is relative
+        // to the root directory of which the command was invoked, so we use the gruntConfigPath to find the correct
+        // path inside of grunt-config.
+        cacheFile: `${ gruntConfigPath }/eslint/cache/${ md5( repo ) }.eslintcache`,
+
+        // Indicates where the custom rules are. This path is relative to the root directory of which the command was
+        // invoked, so we use the gruntConfigPath to find the correct rules path located inside of grunt-config
+        rulePaths: [ `${ gruntConfigPath }/eslint/rules` ],
+
+        // Files and directories to skip when linting.
+        ignorePattern: IGNORE_PATTERN
+      } );
+
+      //----------------------------------------------------------------------------------------
+      // Log results
+      grunt.log.writeln( `Linting ${ currentWorkingDirectory } ...` );
+
+      // Run the ESlint step
+      const report = cli.executeOnFiles( currentWorkingDirectory );
+
+      // Pretty print results to console if any
+      ( report.warningCount || report.errorCount ) && grunt.log.write( cli.getFormatter()( report.results ) );
+      report.warningCount && grunt.fail.warn( report.warningCount + ' Lint Warnings' );
+      report.errorCount && grunt.fail.fatal( report.errorCount + ' Lint Errors' );
+    };
   }
 
   return Linter;
