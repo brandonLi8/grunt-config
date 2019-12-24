18d0e462ff4863f4d3a43d203a47845cafd70c66
Brandon Li <brandon.li820@icloud.com>
2019-12-23 18:13:48 -0700

    #20 Add the labels schema getter

--- src/Labeler.js
+++ src/Labeler.js
@@ -1,115 +1,30 @@
 // Copyright © 2019 Brandon Li. All rights reserved.
 
 /**
- * Array of Github labels.
- *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
 
-module.exports = [
-  {
-    name: 'design:general',
-    color: 'FF2DA6'
-  },
-  {
-    name: 'design:polish',
-    color: 'F13AFA'
-  },
-  {
-    name: 'dev:chip-away',
-    color: '5C8EFF'
-  },
-  {
-    name: 'dev:code-review',
-    color: '312AFF'
-  },
-  {
-    name: 'dev:enhancement',
-    color: '299BFF',
-    aliases: [ 'enhancement' ]
-  },
-  {
-    name: 'dev:help-wanted',
-    color: '63D1F4',
-    aliases: [ 'help-wanted' ]
-  },
-  {
-    name: 'dev:maintenance-release',
-    color: '1467ba'
-  },
-  {
-    name: 'dev:model',
-    color: '4876FF'
-  },
-  {
-    name: 'priority:1-top',
-    color: 'FF007F'
-  },
-  {
-    name: 'priority:2-high',
-    color: 'e11d21'
-  },
-  {
-    name: 'priority:3-medium',
-    color: 'FFE600'
-  },
-  {
-    name: 'priority:4-low',
-    color: 'c7def8'
-  },
-  {
-    name: 'priority:5-deferred',
-    color: 'ACFAAE'
-  },
-  {
-    name: 'project:master-checklist',
-    color: '42426F'
-  },
-  {
-    name: 'status:blocks-publication',
-    color: 'FF2828'
-  },
-  {
-    name: 'status:ready-for-review',
-    color: '3E766D'
-  },
-  {
-    name: 'type:automated-testing',
-    color: 'FF00AA'
-  },
-  {
-    name: 'type:bug',
-    color: 'db434b',
-    aliases: [ 'bug' ]
-  },
-  {
-    name: 'type:duplicate',
-    color: 'EEEED1',
-    aliases: [ 'duplicate' ]
-  },
-  {
-    name: 'type:licensing',
-    color: '4B0082'
-  },
-  {
-    name: 'type:misc',
-    color: 'B87333'
-  },
-  {
-    name: 'type:responsive-design',
-    color: 'BA71FF'
-  },
-  {
-    name: 'type:performance',
-    color: '8B1C62'
-  },
-  {
-    name: 'type:question',
-    color: 'EDCB62',
-    aliases: [ 'question' ]
-  },
-  {
-    name: 'type:user-feedback',
-    color: 'EDE162'
+module.exports = ( () => {
+  'use strict';
+
+  // modules
+  const Util = require( './Util' );
+  const Generator = require( './Generator' );
+
+  // constants
+
+  // Object literal that describes the Github issue labels that should be added. Each key is the name of the Github
+  // issue label and correlates to one of the two values stated below:
+  // 1. String - The color of the Github label as it appears in the issue. The label is assumed to have no aliases.
+  // 2. Object Literal - an object literal with:
+  //                      - a color key that correlates to a color string as described in 1.
+  //                      - a string array that contains aliases to other labels
+  const LABELS_SCHEMA = grunt.file.readJSON( path.dirname( __dirname ) + '/github-labels-schema.json';
+
+
+  class Labeler {
+
   }
-];
+
+  return Labeler;
+} )();
