16567f64a67328ddd3d1a08d02ae9a5b549e9a0a
Brandon Li <brandon.li820@icloud.com>
2019-12-27 00:23:19 -0700

    #20 finish cleaning up copyright.js

--- eslint/rules/copyright.js
+++ eslint/rules/copyright.js
@@ -1,7 +1,8 @@
 // Copyright © 2019 Brandon Li. All rights reserved.
 
 /**
- * Custom ESlint rule that checks the first line of each file is a copyright statement and is correctly implemented.
+ * Custom ESlint rule that enforces the first line of each file to be a correctly implemented copyright statement,
+ * including format and dates.
  *
  * See ../../src/Copyright.js (relative to this file) for more documentation of a valid copyright statement.
  * Validates by generating a correct copyright statement and comparing with the current copyright statement.
@@ -22,14 +23,13 @@ module.exports = ( () => {
 
     // Meta-data
     meta: {
-      type: 'problem',
+      type: 'suggestion',
       docs: {
-        description: 'Enforces correct copyright statements. See grunt-config/src/Copyright.js for documentation.',
-        category: 'Best Practices',
+        description: 'Enforces correct copyright statements, including format and dates.',
+        url: 'https://github.com/brandonLi8/grunt-config/blob/master/eslint/rules/copyright.js',
+        category: 'Stylistic Issues',
         recommended: true
-      },
-      fixable: 'code', // not fixable
-      schema: [] // no options
+      }
     },
 
     /**
@@ -38,15 +38,15 @@ module.exports = ( () => {
      *
      * @param {Object} context - Object literal that contains information relevant to the rule. See
      *                           https://eslint.org/docs/developer-guide/working-with-rules
-     * @returns {Object} - Object literal with methods that ESlint calls to visit nodes while traversing the AST
+     * @returns {Object} - Object literal with methods that ESlint calls to visit nodes while traversing through the AST
      */
-    create: context => {
+    create( context ) {
 
       return {
 
         /**
          * Checks that the first line of each file is a copyright statement and is correctly implemented.
-         * Called at the start of every file.
+         * Called at the start of every file when traversing down the AST.
          * @public
          *
          * @param {ASTNode} node - the current node (of the file)
@@ -71,5 +71,4 @@ module.exports = ( () => {
       };
     }
   };
-
 } )();
