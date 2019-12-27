4fcc6be0f90822573f94024141347731d0c171b8
Brandon Li <brandon.li820@icloud.com>
2019-12-27 00:19:07 -0700

    #20 Finish cleaning up require-statement-match

--- eslint/rules/require-statement-match.js
+++ eslint/rules/require-statement-match.js
@@ -27,14 +27,13 @@ module.exports = ( () => {
 
     // Meta-data
     meta: {
-      type: 'problem',
+      type: 'suggestion',
       docs: {
         description: 'Enforces variable names and module names of require statements to match.',
-        category: 'Best Practices',
+        url: 'https://github.com/brandonLi8/grunt-config/blob/master/eslint/rules/require-statement-match.js',
+        category: 'Stylistic Issues',
         recommended: true
-      },
-      fixable: 'code', // not fixable
-      schema: [] // no options
+      }
     },
 
     /**
@@ -43,37 +42,34 @@ module.exports = ( () => {
      *
      * @param {Object} context - Object literal that contains information relevant to the rule. See
      *                           https://eslint.org/docs/developer-guide/working-with-rules
-     * @returns {Object} - Object literal with methods that ESlint calls to visit nodes while traversing the AST
+     * @returns {Object} - Object literal with methods that ESlint calls to visit nodes while traversing through the AST
      */
-    create: context => {
+    create( context ) {
 
       return {
 
         /**
-         * Checks that variable names and module names of require statements to match. No-op if the variable declaration
-         * isn't a require statement.
+         * Called when traversing down the AST at each variable declaration. If the variable declaration is a require
+         * statement, this checks that variable name and module name exactly match. No-op for non require statement
+         * variable declarations.
          * @public
          *
          * @param {ASTNode} node - the current node (a variable declaration)
          */
         VariableDeclaration( node ) {
 
-          // Check every variable declaration.
+          // Check every variable declaration to allow multiple variable declarations.
           node.declarations.forEach( declarationNode => {
 
-            // First check if the variable declaration is a require statement. A typical require statement node has the
-            // following properties that we care about:
-            // *|   id: {
-            // *|     name: 'VariableName'
-            // *|   },
-            // *|   init: {
-            // *|     callee: { name: 'require' },
-            // *|     arguments: [ {
-            // *|       value: 'ModuleName'
-            // *|     } ]
-            // *|   }
-            // This corresponds with `const VariableName = require( 'ModuleName' );`
-            // A node with the name property and the argument callee as 'require' is a require statement.
+            // First check if the variable declaration is a require statement. A typical require statement node for
+            // `const VariableName = require( 'foo/barModuleName' );` has the following properties that we care about:
+            //   *|  id: { name: 'VariableName' },
+            //   *|  init: {
+            //   *|    callee: { name: 'require' },
+            //   *|    arguments: [ {
+            //   *|      value: 'foo/bar/ModuleName'
+            //   *|    } ]
+            //   *|  }
             if ( declarationNode.init &&
                  declarationNode.id &&
                  declarationNode.init.callee &&
@@ -83,10 +79,11 @@ module.exports = ( () => {
               const variableName = declarationNode.id.name;
               const requireStatementPath = declarationNode.init.arguments[ 0 ].value;
 
-              // Get the module name from the requireStatementPath. For instance, turn foo/bar/ModuleName to ModuleName.
+              // Get the module name from the last path of the requireStatementPath. In the example above, this would
+              // turn 'foo/bar/ModuleName' into 'ModuleName'.
               const moduleName = requireStatementPath.substring( requireStatementPath.lastIndexOf( '/' ) + 1 );
 
-              if ( variableName !== moduleName ) {
+              if ( variableName !== moduleName ) {  // exact (case sensitive) match check
                 context.report( {
                   node,
                   loc: node.loc.start,
