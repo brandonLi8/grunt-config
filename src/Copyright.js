6443af2459075c078305621d7fe9bc87af218202
Brandon Li <brandon.li820@icloud.com>
2020-01-03 19:45:50 -0700

    use os.EOL

--- src/Copyright.js
+++ src/Copyright.js
@@ -124,9 +124,6 @@ module.exports = ( () => {
       // Reference the correct copyright statement,
       const copyrightStatement = this.getFileCopyright( filePath );
 
-      // no-op if the first line is already correct.
-      if ( fileLines[ 0 ] === copyrightStatement ) return;
-
       // Only replace the first line if it was already a copyright statement by checking if the word "copyright" is in
       // the first line or if forceWrite is true
       if ( forceWrite || fileLines[ 0 ].toLowerCase().indexOf( 'copyright' ) >= 0 ) {
