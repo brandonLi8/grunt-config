895d02cca2257c20a8831c7573c592e0a7d430fa
Brandon Li <brandon.li820@icloud.com>
2020-01-11 20:54:44 -0700

    #22 clean up build require step

--- src/Builder.js
+++ src/Builder.js
@@ -72,131 +72,55 @@ module.exports = ( () => {
      * @param {Object} [options]
      * @returns {Promise.<string>} - The combined JS output from the optimizer
      */
-    // static optimizeAMD( configFile, options ) {
-    //   options = {
-    //     // {Object|boolean} - See https://github.com/requirejs/r.js/blob/master/build/example.build.js for the wrap
-    //     // documentation, as it lists all of the available options.
-    //     wrap: false,
-
-    //     insertRequire: false,
-    //     ...options
-    //   };
-    //   let output;
-
-    //   // All options are documented at https://github.com/requirejs/r.js/blob/master/build/example.build.js
-    //   const config = {
-
-    //     appDir: '../sim-core/src',
-    //     baseUrl: './',
-
-    //     // Includes a require.js stub called almond, so that we don't have to include the full require.js runtime
-    //     // inside of builds. This helps reduce file size, and the rest of require.js isn't needed.
-    //     // See https://github.com/requirejs/almond for more about specifying name=almond
-    //     name: 'almond',
-    //     optimize: 'none',
-    //     wrap: options.wrap,
-
-    //     // JS config file
-    //     mainConfigFile: configFile,
-
-    //     // optimized output file
-    //     out( js, sourceMap ) {
-    //       console.log( 'erherh')
-    //       output = js;
-    //     },
-
-    //           // Add instrumentation if required
-    //   onBuildWrite: function( moduleName, path, contents ) {
-    //     console.log( moduleName, path, contents, 'asdf' )
-    //   },
-
-    //     // turn off preservation of comments that have a license in them
-    //     preserveLicenseComments: true,
-
-    //     // modules to stub out in the optimized file
-    //     // stubModules: [ 'text', 'image' ],
-    //     // Avoid optimization names that are outside the baseUrl, see http://requirejs.org/docs/optimization.html#pitfalls
-    //     paths: {
-    //       almond: '../../node_modules/almond'
-    //     },
-
-    //     insertRequire: options.insertRequire ? [ options.insertRequire ] : null
-    //   };
-
-    //   Util.assert( grunt.file.exists( configFile ), 'asdfa' + configFile );
-    //   requirejs.optimize( config, buildResponse => {
-    //     Util.log( `require.js optimization complete (${output.length} bytes)` );
-    //     return output;
-    //   }, err => {
-    //     Util.throw( 'asdfasdf' + err );
-    //   } );
-    // }
-    /**
-     * Runs a require.js optimizer build step.
-     * @public
-     *
-     * @param {string} repo
-     * @param {string} mainConfigFile - path to the config file
-     * @param {Object} [options]
-     * @returns {Promise.<string>} - The combined JS output from the optimizer
-     */
-    static optimizeAMD(  mainConfigFile, options ) {
-
-      const {
+    static optimizeAMD( configFile, options ) {
+      options = {
         // {Object|boolean} - See https://github.com/requirejs/r.js/blob/master/build/example.build.js for the wrap
         // documentation, as it lists all of the available options.
-        wrap = false,
+        wrap: false,
 
-        insertRequire = false
-      } = options || {};
+        insertRequire: false,
+        ...options
+      };
+      let output;
+      // All options are documented at https://github.com/requirejs/r.js/blob/master/build/example.build.js
+      const config = {
 
-      /**
-       * Runs the require.js optimizer to determine and combine all of the dependent .js files.
-       * @public
-       *
-       * @returns {Promise} - Resolves with js: {string}
-       */
+
+        // Includes a require.js stub called almond, so that we don't have to include the full require.js runtime
+        // inside of builds. This helps reduce file size, and the rest of require.js isn't needed.
+        // See https://github.com/requirejs/almond for more about specifying name=almond
+        name: 'almond',
+        optimize: 'none',
+        wrap: options.wrap,
+
+        // Avoid optimization names that are outside the baseUrl, see http://requirejs.org/docs/optimization.html#pitfalls
+        paths: {
+          almond: process.cwd() + '/node_modules/almond/almond'
+        },
+
+        // JS config file
+        mainConfigFile: configFile,
+
+        onBuildRead: ( moduleName, path, contents ) => {
+          return this.transpile( contents );
+        },
+
+        // optimized output file
+        out( js, sourceMap ) {
+          output = js;
+        },
+
+        // turn on preservation of comments that have a license in them
+        preserveLicenseComments: true,
+
+        // modules to stub out in the optimized file
+        stubModules: [ 'text', 'image' ],
+
+        insertRequire: options.insertRequire ? [ options.insertRequire ] : null
+      };
+
+
       return new Promise( ( resolve, reject ) => {
-        let output;
-
-        // All options are documented at https://github.com/requirejs/r.js/blob/master/build/example.build.js
-        const config = {
-
-          // Includes a require.js stub called almond, so that we don't have to include the full require.js runtime
-          // inside of builds. This helps reduce file size, and the rest of require.js isn't needed. See
-          // https://github.com/phetsims/chipper/issues/277
-          // See https://github.com/requirejs/almond for more about specifying name=almond
-          name: 'almond',
-
-          optimize: 'none',
-
-          wrap: wrap,
-
-          // Avoid optimization names that are outside the baseUrl, see http://requirejs.org/docs/optimization.html#pitfalls
-          paths: {
-            almond: '../../grunt-config/node_modules/almond/almond'
-          },
-
-          // JS config file
-          mainConfigFile: mainConfigFile,
-
-          onBuildRead: ( moduleName, path, contents ) => {
-            return this.transpile( contents );
-          },
-
-          // optimized output file
-          out( js, sourceMap ) {
-            output = js;
-          },
-
-          // turn off preservation of comments that have a license in them
-          preserveLicenseComments: false
-        };
-
-        if ( insertRequire ) {
-          config.insertRequire = [ insertRequire ];
-        }
-
 
         requirejs.optimize( config, function( buildResponse ) {
           resolve( output );
@@ -204,7 +128,7 @@ module.exports = ( () => {
           reject( new Error( err ) );
         } );
       } );
-    };
+    }
 
     /**
      * Transpiles code from ES6+ to browser-compatible JavaScript for older browsers or environments (ES5) using Babel.
@@ -220,7 +144,7 @@ module.exports = ( () => {
       return babel.transform( code, {
         // Avoids a warning that this gets disabled for >500kb of source.
         compact: true,
-        plugins: [ '@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-classesa' ],
+        plugins: [ '@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-classes' ],
         presets: [ [ `${ process.cwd() }/node_modules/babel-preset-env`, {
 
           // Parse as "script" type, so "this" will refer to "window" instead of being transpiled to `void 0` aka undefined
