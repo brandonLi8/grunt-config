35a012e0df8653128d8a67d55f474ae832a5920a
Brandon Li <brandon.li820@icloud.com>
2020-01-18 11:59:24 -0700

    #22 doc

--- src/UserConfig.js
+++ src/UserConfig.js
@@ -1,14 +1,15 @@
 // Copyright © 2019-2020 Brandon Li. All rights reserved.
 
 /**
- * Utility for referencing, validating, and parsing user-specific configuration options.
+ * Utility class for referencing, validating, and parsing user-specific configuration values.
  *
- * User-specific configuration options are generalized values that depend on the user and environment. For instance,
+ * User-specific configuration values are generalized and depend on the user and the environment. For instance,
  * each package.json of every user will be slightly different. Each key of package.json is a user-specific option and
  * determines Generator values.
  *
  * Contains methods for validating user configuration value(s). These are placed into methods so that they are only
- * validated on the command that needs the value(s).
+ * validated on the command that needs the value(s) instead of every time this module is loaded. If any configuration
+ * options weren't implemented correctly, will provide a helpful error message to guide the user to correct it.
  *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
