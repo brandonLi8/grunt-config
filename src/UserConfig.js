78d677c5fca1a87e668710c4110bda048763793a
Brandon Li <brandon.li820@icloud.com>
2020-01-18 17:49:25 -0700

    #22 doc

--- src/UserConfig.js
+++ src/UserConfig.js
@@ -3,13 +3,14 @@
 /**
  * Utility class for referencing, validating, and parsing user-specific configuration values.
  *
- * User-specific configuration values are generalized and depend on the user and the environment. For instance,
- * each package.json of every user will be slightly different. Each key of package.json is a user-specific option and
- * determines Generator values.
+ * User-specific configuration values are dynamic and depend on the user and its environment. For instance, the
+ * package.json of every user will have slightly different values. Each key-value of package.json is a user-specific
+ * value and determines Generator values.
  *
- * Contains methods for validating user configuration value(s). These are placed into methods so that they are only
- * validated on the command that needs the value(s) instead of every time this module is loaded. If any configuration
- * options weren't implemented correctly, will provide a helpful error message to guide the user to correct it.
+ * Contains static methods for validating user configuration value(s). These are placed into methods so that they are
+ * only validated on the grunt command that needs the value(s) instead of every time this module is loaded. If any
+ * configuration option(s) weren't implemented correctly, will provide a helpful error message to guide the user to
+ * correct it.
  *
  * @author Brandon Li <brandon.li820@gmail.com>
  */
